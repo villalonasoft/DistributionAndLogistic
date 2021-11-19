24c0f06898d97ef67423b78a1a1c20a6bcef40be
ArielVillalona <a.villalona.reyes@hotmail.com>
2021-11-19 06:21:44 -0400

    init, menu, submenu, refactor screen

---
+++ src/app/shared/menu-items/menu-items.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+
+export interface Parent{
+  name:string,
+  submenu:Menu[]
+}
+export interface Menu {
+  state: string;
+  name: string;
+  type: string;
+  icon: string;
+}
+
+const MENUITEMS = [
+  {name:"Entrada De Datos",
+  submenu:[
+    { state: 'dashboard', name: 'Home', type: 'link', icon: 'av_timer'},
+    { state: 'button', type: 'link', name: 'Buttons', icon: 'crop_7_5'},
+    { state: 'grid', type: 'link', name: 'Grid List', icon: 'view_comfy'},
+    { state: 'lists', type: 'link', name: 'Lists', icon: 'view_list'},
+    { state: 'menu', type: 'link', name: 'Menu', icon: 'view_headline'},
+    { state: 'tabs', type: 'link', name: 'Tabs', icon: 'tab'},
+    { state: 'stepper', type: 'link', name: 'Stepper', icon: 'web'},
+  ]
+  },
+  {name:"Otros",
+  submenu:[
+    {
+      state: 'expansion',
+      type: 'link',
+      name: 'Expansion Panel',
+      icon: 'vertical_align_center'
+    },
+    { state: 'chips', type: 'link', name: 'Chips', icon: 'vignette'},
+    { state: 'toolbar', type: 'link', name: 'Toolbar', icon: 'voicemail'},
+    {
+      state: 'progress-snipper',
+      type: 'link',
+      name: 'Progress snipper',
+      icon: 'border_horizontal'
+    },
+    {
+      state: 'progress',
+      type: 'link',
+      name: 'Progress Bar',
+      icon: 'blur_circular'
+    },
+    {
+      state: 'dialog',
+      type: 'link',
+      name: 'Dialog',
+      icon: 'assignment_turned_in'
+    },
+    { state: 'tooltip', type: 'link', name: 'Tooltip', icon: 'assistant',child: 'fill' },
+    { state: 'snackbar', type: 'link', name: 'Snackbar', icon: 'adb',child: 'fill' },
+    { state: 'slider', type: 'link', name: 'Slider', icon: 'developer_mode'},
+    {
+      state: 'slide-toggle',
+      type: 'link',
+      name: 'Slide Toggle',
+      icon: 'all_inclusive'
+    }
+  ]
+  },
+];
+
+@Injectable()
+export class MenuItems {
+  getMenuitem(): Parent[] {
+    return MENUITEMS;
+  }
+}
