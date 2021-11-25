c1c4415095c7b7cbe6016187ac83e85bc1700d09
Ariel Villalona <avillalona@farmatrix.com.do>
2021-11-25 14:39:55 -0400

    register menu item

---
+++ src/app/shared/menu-items/menu-client.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+export interface Parent{
+  name:string,
+  submenu:Menu[]
+}
+export interface Menu {
+  state: string;
+  name: string;
+  type: string;
+  icon: string;
+}
+
+const MENUCLIENT = [
+      { state: 'orders',type: 'link', name: 'Generar Pedido',  icon: 'av_timer'},
+      { state: 'consult',type: 'link', name: 'Consultar Estado',  icon: 'av_timer'}
+];
+
+@Injectable()
+export class MenuClient {
+  getMenuitem(): Menu[] {
+    return MENUCLIENT;
+  }
+}
