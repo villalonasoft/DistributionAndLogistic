21b23c727f0688ba2e01843668b3f0f8c55012b2
ArielVillalona <a.villalona.reyes@hotmail.com>
2021-11-28 20:45:42 -0400

    reorganize items

--- src/app/shared/menu-items/menu-items.ts
+++ src/app/shared/menu-items/menu-items.ts
@@ -24,57 +24,49 @@ const MENUITEMS = [
       { state: 'supplier', type: 'link', name: 'Supplidores', icon: 'web'},
       { state: 'department', type: 'link', name: 'Departamento', icon: 'web'},
       { state: 'brand', type: 'link', name: 'Marcas', icon: 'web'},
+      { state: 'brand', type: 'link', name: 'Clientes', icon: 'web'},
+      { state: 'brand', type: 'link', name: 'Almacenes', icon: 'web'},
     ]
   },
   {
     name:"Transacciones",
     submenu:[
-      { state: 'dashboard', name: 'Home', type: 'link', icon: 'av_timer'},
-      { state: 'button', type: 'link', name: 'Buttons', icon: 'crop_7_5'},
-      { state: 'grid', type: 'link', name: 'Grid List', icon: 'view_comfy'},
-      { state: 'lists', type: 'link', name: 'Lists', icon: 'view_list'},
-      { state: 'menu', type: 'link', name: 'Menu', icon: 'view_headline'},
-      { state: 'tabs', type: 'link', name: 'Tabs', icon: 'tab'},
-      { state: 'stepper', type: 'link', name: 'Stepper', icon: 'web'},
+      { state: 'brand', type: 'link', name: 'Act. de precios', icon: 'web'},
+      { state: 'dashboard', name: 'Gestionar Ordenes', type: 'link', icon: 'av_timer'},
+      { state: 'button', type: 'link', name: 'Recepcion de pedidos', icon: 'crop_7_5'},
+      { state: 'grid', type: 'link', name: 'Ajustes de inventario', icon: 'view_comfy'},
+      { state: 'lists', type: 'link', name: 'Facturacion', icon: 'view_list'},
+      { state: 'menu', type: 'link', name: 'Despacho', icon: 'view_headline'},
+      { state: 'tabs', type: 'link', name: 'Rutas', icon: 'tab'},
+      { state: 'stepper', type: 'link', name: 'Ubicacion de productos', icon: 'web'},
     ]
   },
   {
     name:"Reportes",
     submenu:[
-      { state: 'presentation',type: 'link', name: 'Presentaciones',  icon: 'av_timer'},
-      { state: 'product', type: 'link', name: 'Productos', icon: 'crop_7_5'},
-      { state: 'customer', type: 'link', name: 'Clientes', icon: 'view_comfy'},
-      { state: 'pickers', type: 'link', name: 'Pickers', icon: 'view_list'},
-      { state: 'units', type: 'link', name: 'Unidades', icon: 'view_headline'},
-      { state: 'taxes', type: 'link', name: 'Impuestos', icon: 'tab'},
-      { state: 'supplier', type: 'link', name: 'Supplidores', icon: 'web'},
+      { state: 'presentation',type: 'link', name: 'Cambios de costo',  icon: 'av_timer'},
+      { state: 'product', type: 'link', name: 'Ventas', icon: 'crop_7_5'},
+      { state: 'customer', type: 'link', name: 'Inventario', icon: 'view_comfy'},
+      { state: 'pickers', type: 'link', name: 'Pedidos', icon: 'view_list'},
+      { state: 'units', type: 'link', name: 'Cliente-Pedido', icon: 'view_headline'},
+      { state: 'taxes', type: 'link', name: 'Compras', icon: 'tab'},
+      { state: 'supplier', type: 'link', name: 'Productos', icon: 'web'},
       { state: 'brand', type: 'link', name: 'Marcas', icon: 'web'},
     ]
   },
   {
     name:"Controles",
     submenu:[
-      { state: 'presentation',type: 'link', name: 'Presentaciones',  icon: 'av_timer'},
-      { state: 'product', type: 'link', name: 'Productos', icon: 'crop_7_5'},
-      { state: 'customer', type: 'link', name: 'Clientes', icon: 'view_comfy'},
-      { state: 'pickers', type: 'link', name: 'Pickers', icon: 'view_list'},
-      { state: 'units', type: 'link', name: 'Unidades', icon: 'view_headline'},
-      { state: 'taxes', type: 'link', name: 'Impuestos', icon: 'tab'},
-      { state: 'supplier', type: 'link', name: 'Supplidores', icon: 'web'},
-      { state: 'brand', type: 'link', name: 'Marcas', icon: 'web'},
+      { state: 'presentation',type: 'link', name: 'Reubicar Producto',  icon: 'av_timer'},
+      { state: 'product', type: 'link', name: 'Dividir zona', icon: 'crop_7_5'},
+      { state: 'customer', type: 'link', name: 'Reasignar pedido', icon: 'view_comfy'},
+      { state: 'pickers', type: 'link', name: 'Mover Pedido', icon: 'view_list'},
     ]
   },
   {
     name:"Utilitarios",
     submenu:[
       { state: 'presentation',type: 'link', name: 'Presentaciones',  icon: 'av_timer'},
-      { state: 'product', type: 'link', name: 'Productos', icon: 'crop_7_5'},
-      { state: 'customer', type: 'link', name: 'Clientes', icon: 'view_comfy'},
-      { state: 'pickers', type: 'link', name: 'Pickers', icon: 'view_list'},
-      { state: 'units', type: 'link', name: 'Unidades', icon: 'view_headline'},
-      { state: 'taxes', type: 'link', name: 'Impuestos', icon: 'tab'},
-      { state: 'supplier', type: 'link', name: 'Supplidores', icon: 'web'},
-      { state: 'brand', type: 'link', name: 'Marcas', icon: 'web'},
     ]
   },
   {
@@ -84,38 +76,11 @@ const MENUITEMS = [
       {
         state: 'expansion',
         type: 'link',
-        name: 'Expansion Panel',
+        name: 'Base de conocimiento',
         icon: 'vertical_align_center'
       },
-      { state: 'chips', type: 'link', name: 'Chips', icon: 'vignette'},
-      { state: 'toolbar', type: 'link', name: 'Toolbar', icon: 'voicemail'},
-      {
-        state: 'progress-snipper',
-        type: 'link',
-        name: 'Progress snipper',
-        icon: 'border_horizontal'
-      },
-      {
-        state: 'progress',
-        type: 'link',
-        name: 'Progress Bar',
-        icon: 'blur_circular'
-      },
-      {
-        state: 'dialog',
-        type: 'link',
-        name: 'Dialog',
-        icon: 'assignment_turned_in'
-      },
-      { state: 'tooltip', type: 'link', name: 'Tooltip', icon: 'assistant',child: 'fill' },
-      { state: 'snackbar', type: 'link', name: 'Snackbar', icon: 'adb',child: 'fill' },
-      { state: 'slider', type: 'link', name: 'Slider', icon: 'developer_mode'},
-      {
-        state: 'slide-toggle',
-        type: 'link',
-        name: 'Slide Toggle',
-        icon: 'all_inclusive'
-      }
+      { state: 'chips', type: 'link', name: 'Documentacion', icon: 'vignette'},
+      { state: 'chips', type: 'link', name: 'Leira Asistent', icon: 'vignette'},
     ]
   },
 ];
