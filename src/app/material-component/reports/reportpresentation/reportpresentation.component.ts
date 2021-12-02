c2d1bc9f244357074cd69ce907f586718c6cd05a
Ariel Villalona <avillalona@farmatrix.com.do>
2021-12-02 15:03:38 -0400

    Report table sort and filter

--- src/app/material-component/reports/reportpresentation/reportpresentation.component.ts
+++ src/app/material-component/reports/reportpresentation/reportpresentation.component.ts
@@ -1,5 +1,6 @@
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
@@ -8,17 +9,27 @@ import {MatTableDataSource} from '@angular/material/table';
   styleUrls: ['./reportpresentation.component.css']
 })
 export class ReportPresentationComponent implements AfterViewInit {
-  displayedColumns: string[] = ['name', 'codigo', 'departamento', 'marca','suplidor','costo','margen','itbis','precio','factor','unidad'];
+  displayedColumns: string[] = ['codigo', 'name', 'departamento', 'marca','suplidor','costo','margen','itbis','precio','factor','unidad'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
 
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
+  applyFilter(filterValue:string){
+    filterValue = filterValue.trim().toLocaleLowerCase();
+    console.log(filterValue);
+  }
 }
+
 export interface PeriodicElement {
   name: string;
   codigo: string;
@@ -35,25 +46,25 @@ export interface PeriodicElement {
 
 const ELEMENT_DATA: PeriodicElement[] = [
   {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534.00,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
-  {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015262', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015263', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015264', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015265', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015266', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015267', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015268', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015269', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015210', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015211', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015212', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015213', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015214', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015215', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015216', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015217', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015118', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015219', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
+  {codigo: '00015220', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
   {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
   {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
   {codigo: '00015261', name: 'Hydrogen',departamento:"salud",marca:"Generic",suplidor:"Sued",costo:1000.00,margen:30,itbis:18,precio:1534,factor: 1,unidad:"caja"},
